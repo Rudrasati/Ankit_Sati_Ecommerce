7cdb81d726ef86410a0c98966988670225694a4c
Rudrasati <85786895+Rudrasati@users.noreply.github.com>
2023-05-21 02:00:38 +0530

    E-commerce single page application

---
+++ ecommerce/src/components/Banner.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Image from "next/image";
+
+function Banner(){
+    return (
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <Image src="/images/clothing.jpg" className="d-block w-100" width={600} height={460} alt="banner slide 1"/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/headphone.jpg" className="d-block w-100" width={600} height={460} alt="banner slide 2"/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide lab el</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/laptops.jpg" className="d-block w-100" width={600} height={460} alt="banner slide 3"/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide lab el</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/smartphones.jpg" className="d-block w-100" width={600} height={460} alt="banner slide 4"/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide lab el</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/watches.jpg" className="d-block w-100" width={600} height={460} alt="banner slide 5"/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+    )
+}
+export default Banner
